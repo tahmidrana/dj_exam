d7c1e5470c075bdfca3679aa0d399e19a64842ae
tahmidrana <tahmidrana@gmail.com>
2018-11-28 23:29:02 +0600

    deployed to heroku

--- dj_exam/settings.py
+++ dj_exam/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "*#s&_&+lkjqob8lltlog+zyjsd4m%y@(mis&96l!s-u*oap7jy"
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "*#s&_&+lkjqob8lltlog+zyjsd4m%y@(mis&96l!s-u*oap7jy"
+# SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config("DEBUG", default=False, cast=bool)
+# DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
